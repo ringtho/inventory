0406d5581bb591806371e50fcd2edbfc05ed34b7
Smith Ringtho <sringtho@gmail.com>
2024-11-01 07:52:55 +0300

    Added: helper function to convert to JSON using chi

---
+++ helpers/json.go
@@ -0,0 +1,30 @@
+package helpers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// JSON is a helper function to write JSON responses
+func JSON(w http.ResponseWriter, status int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(data)
+}
+
+// respondWithError is a helper function to write error responses
+func respondWithError(w http.ResponseWriter, status int, message string){
+
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	JSON(w, status, errorResponse{Error: message})
+}
--- routers/router.go
+++ routers/router.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/ringtho/inventory/helpers"
 )
 
 
@@ -13,11 +14,17 @@ func Router() http.Handler {
 	router.Use(middleware.Logger)
 
 	apiRouter := chi.NewRouter()
+
+	
 	apiRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Smith Ringtho!"))
+		type Message struct {
+			Message string `json:"message"`
+		}
+		message  := Message{ Message: "Welcome to the Inventory API"}
+		helpers.JSON(w, 200, message)
 	})
 
-	router.Mount("/api/v1/", apiRouter)
+	router.Mount("/api/v1", apiRouter)
 
 	return router
 }
